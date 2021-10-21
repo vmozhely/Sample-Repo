bedcf1850465b3f8f27c560febeac4f71921dac6
vmozhely <vmozhely@uncc.edu>
2021-10-21 00:44:46 -0400

    part 23

--- attempt2/src/attempt2/Attempt2.java
+++ attempt2/src/attempt2/Attempt2.java
@@ -15,6 +15,7 @@ public class Attempt2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+System.out.println("Test test");
 System.out.println("Test test");    }
     
 }
